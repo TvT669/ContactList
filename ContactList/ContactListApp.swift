5cc333d418fbe70d3247356d894b4237bf62dc06
bee <3172736837@qq.com>
2025-02-28 21:32:35 +0800

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/2/28.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactListTests/ContactListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListTests.swift
+//  ContactListTests
+//
+//  Created by 珠穆朗玛小蜜蜂 on 2025/2/28.
+//
+
+import Testing
+@testable import ContactList
+
+struct ContactListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
